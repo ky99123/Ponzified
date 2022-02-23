2a98a952b6250bcece0c8bd149d0845b3ba4102f
ky99123 <quahky11@gmail.com>
2022-02-23 15:21:59 +0800

    Test

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return """<form action="" method="get">
-                <input type="text" name="celsius">
-                <input type="submit" value="Convert">
-              </form>"""
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
