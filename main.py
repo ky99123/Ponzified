baea545d2cee1efd266fffb8424199d365246c21
Nigouz <nicolet1224@gmail.com>
2022-03-03 00:18:28 +0800

    still trying

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_nav import Nav
 
 app = Flask(__name__)
+#tell the app where our db is
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
 
+@app.route('/', methods=['POST','GET']) #parse url string of our application
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    if request.method=='POST':
+        return render_template('input.html')
+    else:
+        return render_template('index.html')
+
+def userinput():
+    return render_template('input.html')
+
+#here we define our menu items
+topbar = Navbar(logo,
+                View('News', 'get_news'),
+                View('Live', 'get_live'),
+                View('Programme', 'get_programme'),
+                View('Classement', 'get_classement'),
+                View('Contact', 'get_contact'),
+                )
+
+# registers the "top" menubar
+nav = Nav()
+nav.register_element('top', topbar)
+
+if __name__ == "__main__":
+    app.run(debug=True) #debug true means error show up on the site
